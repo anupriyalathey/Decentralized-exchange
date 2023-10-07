b5fb8d9f612624018df11aced42c4c47cd014ce5
anupriyalathey <anupriyalathey@gmail.com>
2023-10-08 00:56:13 +0530

    added swap page

--- dex/src/components/Swap.js
+++ dex/src/components/Swap.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {Input, Popover, Radio, Modal, message} from 'antd'
+import {
+  ArrowDownOutlined,
+  DownOutlined,
+  SettingOutlined,
+} from '@ant-design/icons';
 
 function Swap() {
-  return (
-    <div>Swap</div>
+  const[slippage, setSlippage] = useState(2.5);
+  const[tokenOneAmount, setTokenOneAmount] = useState(null);
+  const[tokenTwoAmount, setTokenTwoAmount] = useState(null);
+
+  function handleSlippageChange(e) {
+    setSlippage(e.target.value);
+  }
+
+  function changeAmount(e) {
+    setTokenOneAmount(e.target.value);
+  }
+  const settings = (
+    <>
+    <div>Slippage Tolerance</div>
+    <div>
+      <Radio.Group value={slippage} onChange={handleSlippageChange}>
+        <Radio.Button value="0.5">0.5%</Radio.Button>
+        <Radio.Button value="2.5">2.5%</Radio.Button>
+        <Radio.Button value="5">5%</Radio.Button>
+      </Radio.Group>
+    </div>
+    </>
+  )
+    return (
+    <div className="tradeBox">
+      <div className="tradeBoxHeader">
+        <h4>Swap</h4>
+        <Popover content={settings} title="Settings" trigger="click" placement="bottomRight" >
+        <SettingOutlined className="cog" />
+        </Popover>
+      </div>
+      <div className='inputs'>
+        <Input placeholder="0" value={tokenOneAmount} onChange={changeAmount} />
+        <Input placeholder="0" value={tokenTwoAmount} disabled={true} /> {/*  disabled: true since here we'll be using moralis token api to get the price */}
+      </div>
+    </div>
   )
 }
 
