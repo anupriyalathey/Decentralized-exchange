725eb9083865ad30446ba89c7606a2772bb6b484
anupriyalathey <anupriyalathey@gmail.com>
2023-10-07 23:51:03 +0530

    added header

--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import A from "../A.jpeg"
+import Eth from "../eth.svg"
+import { Link } from "react-router-dom";
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className='leftH'>
+        <img src={A} alt='A' className='logo'/>
+        <Link to="/" className='link'>
+        <div className='headerItem'>Swap</div>
+        </Link>
+        <Link to="/tokens" className='link'>
+        <div className='headerItem'>Tokens</div>
+        </Link>      </div>
+      <div className='rightH'>
+        <div className='headerItem'><img src={Eth} alt="eth" className='eth'/>
+        Ethereum
+        </div>
+        <div className='connectButton'>Connect</div>
+      </div> 
+    </header> 
   )
 }
 
