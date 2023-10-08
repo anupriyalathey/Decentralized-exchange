08ab3188590cf6d1f3fd268edc946d97016d169e
anupriyalathey <anupriyalathey@gmail.com>
2023-10-09 02:05:47 +0530

    added token choices

--- dex/src/components/Swap.js
+++ dex/src/components/Swap.js
@@ -5,12 +5,17 @@ import {
   DownOutlined,
   SettingOutlined,
 } from '@ant-design/icons';
+import tokenList from '../tokenList.json'
 
 function Swap() {
   const[slippage, setSlippage] = useState(2.5);
   const[tokenOneAmount, setTokenOneAmount] = useState(null);
   const[tokenTwoAmount, setTokenTwoAmount] = useState(null);
-
+  const [tokenOne, setTokenOne] = useState(tokenList[0]);
+  const [tokenTwo, setTokenTwo] = useState(tokenList[1]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [changeToken, setChangeToken] = useState(1);
+  
   function handleSlippageChange(e) {
     setSlippage(e.target.value);
   }
@@ -18,6 +23,28 @@ function Swap() {
   function changeAmount(e) {
     setTokenOneAmount(e.target.value);
   }
+
+  function switchToken() {
+    const one = tokenOne;
+    const two = tokenTwo;
+    setTokenOne(two);
+    setTokenTwo(one);
+  }
+
+  function openModal(asset) {
+    setChangeToken(asset);
+    setIsOpen(true);
+  }
+
+  function modifyToken(i) {
+    if(changeToken === 1) { // i.e. if token 1 changed
+      setTokenOne(tokenList[i]);
+    } else {
+      setTokenTwo(tokenList[i]);
+    }
+    setIsOpen(false);
+  }
+  
   const settings = (
     <>
     <div>Slippage Tolerance</div>
@@ -31,6 +58,30 @@ function Swap() {
     </>
   )
     return (
+      <> 
+    <Modal 
+      open={isOpen}
+      footer={null}
+      onCancel={() => setIsOpen(false)}
+      title="Select a token"
+    >
+      <div className='modalContent'>
+        {tokenList?.map((e,i)=> {
+          return (
+            <div className='tokenChoice'
+            key={i}
+            onClick={()=>modifyToken(i)}
+            >
+            <img src={e.img} alt={e.ticker} className='tokenLogo' />
+            <div className='tokenChoiceNames'>
+              <div className='tokenName'>{e.name}</div>
+              <div className='tokenTicker'>{e.ticker}</div>
+            </div>
+            </div>
+          )
+        })}
+      </div>
+      </Modal>
     <div className="tradeBox">
       <div className="tradeBoxHeader">
         <h4>Swap</h4>
@@ -41,8 +92,26 @@ function Swap() {
       <div className='inputs'>
         <Input placeholder="0" value={tokenOneAmount} onChange={changeAmount} />
         <Input placeholder="0" value={tokenTwoAmount} disabled={true} /> {/*  disabled: true since here we'll be using moralis token api to get the price */}
-      </div>
+        <div className='switchButton' onClick={switchToken}>
+          <ArrowDownOutlined classNAme="switchArrow" />
+        </div>
+        <div className="assetOne" onClick={() => openModal(1)}>
+          <img src={tokenOne.img} alt="assetOneLogo" className="assetLogo" />
+          {tokenOne.ticker}
+          <DownOutlined />
+        </div>
+        <div className="assetTwo" onClick={() => openModal(2)}>
+          <img src={tokenTwo.img} alt="assetTwoLogo" className="assetLogo" />
+          {tokenTwo.ticker}
+          <DownOutlined />
+        </div>     
     </div>
+    <div className='swapButton' disabled={!tokenOneAmount}> 
+    {/* Swap button disabled when no value in token1 */}
+      Swap
+      </div> 
+    </div>
+    </>  
   )
 }
 
